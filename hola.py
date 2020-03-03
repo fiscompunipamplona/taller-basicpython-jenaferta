30098ca90e2d88760c7127b2df15799aa4d21e95
mafebustos <mafe.bustos09@gmail.com>
2020-03-03 08:52:44 -0500

    trabajo en clase

---
+++ hola.py
@@ -0,0 +1,25 @@
+
+x=1
+y=2
+print (x+y)
+print (x-y)
+print (x*y)
+print (x/y)
+print (x**y)
+print ("page 13")
+print (x+2*y)
+print (x-y/2)
+print (2*x**3)
+print ((x/2)*y)
+print (x, y, sep="...")
+print (x,"\n",y)
+x=input ("entre el valor para x:")
+x=input("")
+x=float(x)
+print (3*x)
+t=input ("ingrese el valor del tiempo t:")
+h=input ("ingrese la altura h:")
+g=9.8
+y=(0.5*g*t**2)+h
+print ("el valor obtenido es",y)
+
